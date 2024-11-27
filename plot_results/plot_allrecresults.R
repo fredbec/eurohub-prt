2056950ba031e38f59daa5f66f8cd8097e61b258
fredbec <f-becker@posteo.de>
2024-11-27 16:47:56 +0100

    adapt to new data structure

--- plot_results/plot_allrecresults.R
+++ plot_results/plot_allrecresults.R
@@ -2,13 +2,14 @@ library(ggplot2)
 library(data.table)
 library(here)
 library(MetBrewer)
+library(stringr)
 
 source(here("R", "utils-ext.R"))
 source(here("ensvssize", "specs.R"))
 
 DT <- `[`
 
-loctargets <- enscomb_specs$loctargets
+loctargets <- enscomb_specs$loctargets #exclude PL and DE for now, since not done yet
 ks <- enscomb_specs$ks
 targetplot <- "Cases"
 enstype <- "median_ensemble"
@@ -16,7 +17,7 @@ enstype <- "median_ensemble"
 if(enstype == "against_stablek3"){
   cmpa <- "stablek3_median_ensemble"
 } else{
-  cmpa <- "EuroCOVIDhub-ensemble"
+  cmpa <- "median-hubreplica"
 }
 
 plot_horizon_label <- function(length.out = 4){
@@ -32,40 +33,37 @@ plot_horizon_label <- function(length.out = 4){
   return(phl[1:length.out])
 }
 
+substrRight <- function(x, n=1){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+
+
 all_pwscores <- NULL
 all_pwscores_med <- vector(mode = "list", length = length(ks))
 #loctargets <- "GBDeaths"
 for(loctarg in loctargets){
   all_pwscores_med <- vector(mode = "list", length = length(ks))
-for (k in ks){
 
-  all_pwscores_med[[k-1]] <- data.table::fread(here("ensvssize", "serverrun", enstype, paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
+    all_pwscores_med <- data.table::fread(here("enscomb-data", "allk-pwscores", enstype, paste0("ens_comb_pwscores", loctarg, "allk.csv")))
 
-  print(k)
-
-  if(nrow(all_pwscores_med[[k-1]])>0){
-    all_pwscores_med[[k-1]] <-  all_pwscores_med[[k-1]] |>
-    DT(, k := k) |>
-    DT(compare_against == cmpa) |>
-    DT(, meanrelskill := mean(scaled_rel_skill), by = "horizon")|>
-    DT(, medrelskill := median(scaled_rel_skill), by = "horizon")|>
-    DT(, minrelskill := min(scaled_rel_skill), by = "horizon")|>
-    DT(, maxrelskill := max(scaled_rel_skill), by = "horizon") |>
-    DT(, q05relskill := quantile(scaled_rel_skill, 0.05), by = "horizon") |>
-    DT(, q95relskill := quantile(scaled_rel_skill, 0.95), by = "horizon") |>
-    DT(, c("model", "horizon", "mean_scores_ratio", "relative_skill", "scaled_rel_skill", "k",
-           "meanrelskill", "medrelskill", "minrelskill", "maxrelskill", "q05relskill", "q95relskill")) |>
-    unique()
-
-    #if(length(grep("mean*", unique(all_pwscores_med[[k-1]]$model))) > 0){
-    #  all_pwscores_med[[k-1]] <- NULL
-    #}
-  }
-}
-
-all_pwscores_med <- rbindlist(all_pwscores_med) |>
-  DT(, location := substr(loctarg, 0, 2)) |>
-  DT(, target_type := substr(loctarg, 3, 8))
+    all_pwscores_med <- all_pwscores_med |>
+      DT(,model := ifelse(model == "median-hubreplica_k0", "median-hubreplica", model)) |>
+      DT(,compare_against := ifelse(compare_against == "median-hubreplica_k0", "median-hubreplica", compare_against)) |>
+      DT(compare_against == cmpa) |>
+      DT(model != cmpa) |>
+      DT(, k := as.numeric(str_extract(model, "(?<=_k)\\d+"))) |>
+      DT(, model := gsub("_k[0-9]*", "", model)) |>
+      DT(, meanrelskill := mean(scaled_rel_skill), by = c("horizon", "k"))|>
+      DT(, medrelskill := median(scaled_rel_skill),  by = c("horizon", "k"))|>
+      DT(, minrelskill := min(scaled_rel_skill),  by = c("horizon", "k"))|>
+      DT(, maxrelskill := max(scaled_rel_skill),  by = c("horizon", "k")) |>
+      DT(, q05relskill := quantile(scaled_rel_skill, 0.05),  by = c("horizon", "k")) |>
+      DT(, q95relskill := quantile(scaled_rel_skill, 0.95),  by = c("horizon", "k")) |>
+      DT(, c("model", "horizon", "mean_scores_ratio", "relative_skill", "scaled_rel_skill", "k",
+             "meanrelskill", "medrelskill", "minrelskill", "maxrelskill", "q05relskill", "q95relskill")) |>
+      unique() |>
+      DT(, location := substr(loctarg, 0, 2)) |>
+    DT(, target_type := substr(loctarg, 3, 8))
 
 all_pwscores <- rbind(all_pwscores, all_pwscores_med)
 }
@@ -81,8 +79,8 @@ colors = met.brewer(name="Hokusai3", n=3)
 
 ltypes <- c("Ensembles mean rel. skill"="solid","Hub Ensemble rel. skill (=1 by definition)"="dashed")
 colfills <- c("min-max range ensembles rel. skill" = 0.25, "q05-q95 range ensembles rel. skill" = 0.5)
-pdf(here("plot_results", paste0("ens_comb_",targetplot, enstype,".pdf")), width = 9.5, height = 12)
-ggplot(data = all_pwscores |> DT(target_type == targetplot)) +
+pdf(here("plot_results", paste0("ens_comb_","allk_fixedcode",".pdf")), width = 9.5, height = 12)
+ggplot(data = all_pwscores) +
 #ggplot(data = all_pwscores) +
   geom_line(aes(x = k, y = meanrelskill, linetype = "Ensembles mean rel. skill", color = location), lwd = 1) +
   geom_ribbon(aes(x = k, ymin = minrelskill, ymax = maxrelskill, alpha = "min-max range ensembles rel. skill", fill = location)) +
