5f154cf888266df2b8c6ea2e755924e9c7c5d00d
fredbec <f-becker@posteo.de>
2024-12-11 15:38:28 +0100

    pwscores, relative to Hub replica ensemble

--- ensvssize/helper/change-to-parquet.R
+++ ensvssize/helper/change-to-parquet.R
@@ -1,20 +1,20 @@
 library(here)
 library(arrow)
 library(data.table)
+library(stringr)
 
 source(here("ensvssize", "specs.R"))
 loctargets <- enscomb_specs$loctargets
-ks <- 2:14
+
+DT <- `[`
 
 for(loctarg in loctargets){
 
   tmp <- data.table::fread(here("enscomb-data", "pwscores", paste0("ens_comb_pwscores", loctarg,".csv")))
+
+  tmp <- tmp |>
+    DT(compare_against == "median-hubreplica")
   arrow::write_parquet(tmp, here("enscomb-data", "pwscores", paste0("ens_comb_pwscores", loctarg,".parquet")))
 
-  #tmp <- data.table::fread(here("enscomb-data", "mean-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
-  #arrow::write_parquet(tmp, here("enscomb-data", "mean-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".parquet")))
-
-  #tmp <- data.table::fread(here("enscomb-data", "hubreplica-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
-  #arrow::write_parquet(tmp, here("enscomb-data", "hubreplica-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".parquet")))
-
+  rm(tmp)
 }
