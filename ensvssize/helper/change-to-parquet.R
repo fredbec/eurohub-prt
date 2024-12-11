ae8b4a6346f3586660e50a695668a576bc010717
fredbec <f-becker@posteo.de>
2024-12-11 12:43:38 +0100

    pwscores, scoring across all k, part 1

--- ensvssize/helper/change-to-parquet.R
+++ ensvssize/helper/change-to-parquet.R
@@ -6,18 +6,15 @@ source(here("ensvssize", "specs.R"))
 loctargets <- enscomb_specs$loctargets
 ks <- 2:14
 
-for(k in ks){
-  for(loctarg in loctargets){
+for(loctarg in loctargets){
 
-    tmp <- data.table::fread(here("enscomb-data", "median-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
-    arrow::write_parquet(tmp, here("enscomb-data", "median-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".parquet")))
+  tmp <- data.table::fread(here("enscomb-data", "pwscores", paste0("ens_comb_pwscores", loctarg,".csv")))
+  arrow::write_parquet(tmp, here("enscomb-data", "pwscores", paste0("ens_comb_pwscores", loctarg,".parquet")))
 
+  #tmp <- data.table::fread(here("enscomb-data", "mean-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
+  #arrow::write_parquet(tmp, here("enscomb-data", "mean-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".parquet")))
 
-    tmp <- data.table::fread(here("enscomb-data", "mean-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
-    arrow::write_parquet(tmp, here("enscomb-data", "mean-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".parquet")))
+  #tmp <- data.table::fread(here("enscomb-data", "hubreplica-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
+  #arrow::write_parquet(tmp, here("enscomb-data", "hubreplica-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".parquet")))
 
-    tmp <- data.table::fread(here("enscomb-data", "hubreplica-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".csv")))
-    arrow::write_parquet(tmp, here("enscomb-data", "hubreplica-pwscores", paste0("ens_comb_pwscores", loctarg, "_k", k, ".parquet")))
-
-  }
 }
