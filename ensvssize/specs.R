8869bb191875bb42aaa0d98d4595092915fa3f92
fredbec <f-becker@posteo.de>
2026-04-27 12:49:23 +0200

    to run results for mean ensemble

--- ensvssize/specs.R
+++ ensvssize/specs.R
@@ -23,7 +23,7 @@ enscomb_specs <- list(
   #"maxens" = 1000000000000,
 
   "horizons" = c(1,2),
-  "ensemble_type" = "median_ensemble",
+  "ensemble_type" = "mean_ensemble",
 
   "with_anomalies" = FALSE
 )
