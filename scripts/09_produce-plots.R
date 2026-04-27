2a3a6533a0c6e4781f6175cf9190e9b79de35c03
fredbec <f-becker@posteo.de>
2026-04-27 12:43:19 +0200

    produce plots for both horizon groups (selection ensemble)

--- scripts/09_produce-plots.R
+++ scripts/09_produce-plots.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(data.table)
 library(dplyr)
 library(patchwork)
 library(knitr)
@@ -135,122 +136,144 @@ overall_plot <-
 
 pdf(here("plot_results", "best_performers_boxplot.pdf"),
     height = 15, width = 12)
-overall_plot
+print(overall_plot)
 dev.off()
 
 
-score_table <- scores_individual |>
-  group_by(model, location, nmod, target_type) |>
-  summarise(avgtg = mean(target_val),
-            avgcr = mean(current_val)) |>
-  mutate(relval = avgtg/avgcr) |>
-  filter(nmod %in% c(5,10)) |>
-  select(model, location, target_type, nmod, relval)
+
+tileplot <- function(horizons = c(1,2)){
+
+  if(length(horizons) == 4){
+    suffix <- "_allhor"
+    range_tileplot <- c(0.6,1.78)
+  } else {
+    suffix <- ""
+    range_tileplot <- c(0.739,1.31)
+  }
+
+  scores_individual <- data.table::fread(here("output", "selection-ensemble", "forecasts", paste0("scores-selection-ens", suffix, ".csv")))
+  scores_average <- data.table::fread(here("output", "selection-ensemble", "forecasts", paste0("avg-scores-selection-ens", suffix, ".csv")))
+
+  score_table <- scores_individual |>
+    group_by(model, location, nmod, target_type) |>
+    summarise(avgtg = mean(target_val),
+              avgcr = mean(current_val)) |>
+    mutate(relval = avgtg/avgcr) |>
+    filter(nmod %in% c(5,10)) |>
+    select(model, location, target_type, nmod, relval)
 
 
-score_table_wide <- score_table |>
-  dplyr::mutate(relval = round(relval,2)) |>
-  mutate(relval = as.character(relval)) |>
-  mutate(relval = ifelse(is.na(relval), "--", relval)) |>
-  mutate(model = factor(model, levels = c("median_ensemble",
-                                          "weighted.median_ensemble",
-                                          "mean_ensemble",
-                                          "weighted.mean_ensemble"),
-                        labels = c("median - unw.",
-                                   "median - weighted",
-                                   "mean - unw.",
-                                   "mean - weighted"),
-                        ordered = TRUE)) |>
-  pivot_wider(names_from = location, values_from = relval) |>
-  arrange(target_type, nmod, model) |>
-  select(target_type, nmod, model, everything())
-#kable(mytab_wide, format = "latex", booktabs = TRUE,
-#      caption = "Performance Values by Location, Target, and Model")
+  score_table_wide <- score_table |>
+    dplyr::mutate(relval = round(relval,2)) |>
+    mutate(relval = as.character(relval)) |>
+    mutate(relval = ifelse(is.na(relval), "--", relval)) |>
+    mutate(model = factor(model, levels = c("median_ensemble",
+                                            "weighted.median_ensemble",
+                                            "mean_ensemble",
+                                            "weighted.mean_ensemble"),
+                          labels = c("median - unw.",
+                                     "median - weighted",
+                                     "mean - unw.",
+                                     "mean - weighted"),
+                          ordered = TRUE)) |>
+    pivot_wider(names_from = location, values_from = relval) |>
+    arrange(target_type, nmod, model) |>
+    select(target_type, nmod, model, everything())
+  #kable(mytab_wide, format = "latex", booktabs = TRUE,
+  #      caption = "Performance Values by Location, Target, and Model")
 
-#new code for plot
-score_table_weighted <- score_table |>
-  filter(grepl("weighted*", model)) |>
-  mutate(model = gsub("weighted.", "", model)) |>
-  mutate(model = ifelse(model == "median_ensemble", "median ens.", "mean ens.")) |>
-  mutate(nmod = paste0("k = ", nmod)) |>
-  mutate(fac2 = paste0(nmod,",\n", model)) |>
-  mutate(location = factor(location,
-                           levels = c("Average", "DE", "PL", "CZ", "FR", "GB"),
-                           labels = c("Average", "Germany", "Poland", "Czech Rep.", "France", "United Kingd.")))
+  #new code for plot
+  score_table_weighted <- score_table |>
+    filter(grepl("weighted*", model)) |>
+    mutate(model = gsub("weighted.", "", model)) |>
+    mutate(model = ifelse(model == "median_ensemble", "median ens.", "mean ens.")) |>
+    mutate(nmod = paste0("k = ", nmod)) |>
+    mutate(fac2 = paste0(nmod,",\n", model)) |>
+    mutate(location = factor(location,
+                             levels = c("Average", "DE", "PL", "CZ", "FR", "GB"),
+                             labels = c("Average", "Germany", "Poland", "Czech Rep.", "France", "United Kingd.")))
 
 
-textsize_y <- 14
+  textsize_y <- 14
 
-plot1 <- ggplot(aes(x = location, y = fac2), data = score_table_weighted) +
-  geom_tile(aes(fill = relval)) +
-  scale_fill_continuous_divergingx("BrBG", mid = 1, limits = c(0.739,1.31), rev = TRUE) + #0.6,1.78 for appendix
-  theme_masterthesis()  %+replace%
-  theme(legend.title = element_blank(),
-        axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust=1),
-        axis.text.y = element_text(size = 12),
-        axis.title.x = element_text(size = textsize_y, angle = 45, vjust = -2),
-        axis.title.y = element_text(size = textsize_y, angle = 90, vjust = 2),
-        strip.text = element_text(size=textsize_y),
-        legend.text=element_text(size=textsize_y-2),
-        plot.margin = margin(t=0,b=0,r=0,l=0, unit = "pt"),
-        plot.title = element_text(hjust = 0.5,
-                                  size = textsize_y + 3,
-                                  vjust = 2),
-        plot.subtitle = element_text(hjust = 0.5,
-                                     size = textsize_y-2,
-                                     vjust = 6)) +
-  geom_text(aes(label = round(relval, 2)), size = 4, color = "black") +
-  facet_wrap(~target_type) +
-  ylab("") +
-  xlab("") +
-  ggtitle("Weighted ensembles")+
-  guides(
-    fill = guide_colorbar(
-      barwidth = 20,  # Width of the color bar (in 'npc' units, normalized plot coordinates)
-      barheight = 1.5 # Height of the color bar
-    ))
+  plot1 <- ggplot(aes(x = location, y = fac2), data = score_table_weighted) +
+    geom_tile(aes(fill = relval)) +
+    scale_fill_continuous_divergingx("BrBG", mid = 1, limits = range_tileplot, rev = TRUE) + #0.6,1.78 for appendix
+    theme_masterthesis()  %+replace%
+    theme(legend.title = element_blank(),
+          axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust=1),
+          axis.text.y = element_text(size = 12),
+          axis.title.x = element_text(size = textsize_y, angle = 45, vjust = -2),
+          axis.title.y = element_text(size = textsize_y, angle = 90, vjust = 2),
+          strip.text = element_text(size=textsize_y),
+          legend.text=element_text(size=textsize_y-2),
+          plot.margin = margin(t=0,b=0,r=0,l=0, unit = "pt"),
+          plot.title = element_text(hjust = 0.5,
+                                    size = textsize_y + 3,
+                                    vjust = 2),
+          plot.subtitle = element_text(hjust = 0.5,
+                                       size = textsize_y-2,
+                                       vjust = 6)) +
+    geom_text(aes(label = round(relval, 2)), size = 4, color = "black") +
+    facet_wrap(~target_type) +
+    ylab("") +
+    xlab("") +
+    ggtitle("Weighted ensembles")+
+    guides(
+      fill = guide_colorbar(
+        barwidth = 20,  # Width of the color bar (in 'npc' units, normalized plot coordinates)
+        barheight = 1.5 # Height of the color bar
+      ))
 
-score_table_unweighted <- score_table |>
-  filter(!grepl("weighted*", model)) |>
-  mutate(model = ifelse(model == "median_ensemble", "median ens.", "mean ens.")) |>
-  mutate(nmod = paste0("k = ", nmod)) |>
-  mutate(fac2 = paste0(nmod,",\n", model)) |>
-  mutate(location = factor(location,
-                           levels = c("Average", "DE", "PL", "CZ", "FR", "GB"),
-                           labels = c("Average", "Germany", "Poland", "Czech Rep.", "France", "United Kingd.")))
+  score_table_unweighted <- score_table |>
+    filter(!grepl("weighted*", model)) |>
+    mutate(model = ifelse(model == "median_ensemble", "median ens.", "mean ens.")) |>
+    mutate(nmod = paste0("k = ", nmod)) |>
+    mutate(fac2 = paste0(nmod,",\n", model)) |>
+    mutate(location = factor(location,
+                             levels = c("Average", "DE", "PL", "CZ", "FR", "GB"),
+                             labels = c("Average", "Germany", "Poland", "Czech Rep.", "France", "United Kingd.")))
 
-plot2 <- ggplot(aes(x = location, y = fac2), data = score_table_unweighted) +
-  geom_tile(aes(fill = relval)) +
-  scale_fill_continuous_divergingx("BrBG", mid = 1, limits = c(0.739,1.31), rev = TRUE) +  #0.6,1.78 for appendix #0.74,1.31 for main
-  theme_masterthesis()  %+replace%
-  theme(legend.title = element_blank(),
-        axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust=1),
-        axis.text.y = element_blank(),
-        axis.title.x = element_text(size = textsize_y, angle = 45, vjust = -2),
-        axis.title.y = element_text(size = textsize_y, angle = 90, vjust = 2),
-        strip.text = element_text(size=textsize_y),
-        legend.text=element_text(size=textsize_y-2),
-        plot.margin = margin(t=0,b=0,r=0,l=0, unit = "pt"),
-        plot.title = element_text(hjust = 0.5,
-                                  size = textsize_y + 3,
-                                  vjust = 2)) +
-  geom_text(aes(label = round(relval, 2)), size = 4, color = "black") +
-  facet_wrap(~target_type) +
-  ylab("") +
-  xlab("") +
-  ggtitle("Equally weighted ensembles") +
-  guides(
-    fill = guide_colorbar(
-      barwidth = 20,  # Width of the color bar (in 'npc' units, normalized plot coordinates)
-      barheight = 1.5 # Height of the color bar
-    ))
+  plot2 <- ggplot(aes(x = location, y = fac2), data = score_table_unweighted) +
+    geom_tile(aes(fill = relval)) +
+    scale_fill_continuous_divergingx("BrBG", mid = 1, limits = range_tileplot, rev = TRUE) +  #0.6,1.78 for appendix #0.74,1.31 for main
+    theme_masterthesis()  %+replace%
+    theme(legend.title = element_blank(),
+          axis.text.x = element_text(size = 12, angle = 45, vjust = 1, hjust=1),
+          axis.text.y = element_blank(),
+          axis.title.x = element_text(size = textsize_y, angle = 45, vjust = -2),
+          axis.title.y = element_text(size = textsize_y, angle = 90, vjust = 2),
+          strip.text = element_text(size=textsize_y),
+          legend.text=element_text(size=textsize_y-2),
+          plot.margin = margin(t=0,b=0,r=0,l=0, unit = "pt"),
+          plot.title = element_text(hjust = 0.5,
+                                    size = textsize_y + 3,
+                                    vjust = 2)) +
+    geom_text(aes(label = round(relval, 2)), size = 4, color = "black") +
+    facet_wrap(~target_type) +
+    ylab("") +
+    xlab("") +
+    ggtitle("Equally weighted ensembles") +
+    guides(
+      fill = guide_colorbar(
+        barwidth = 20,  # Width of the color bar (in 'npc' units, normalized plot coordinates)
+        barheight = 1.5 # Height of the color bar
+      ))
+
+  ovr_plot <- plot1 + plot2 +
+    plot_layout(guides = "collect")  &
+    theme(legend.position = "bottom")
+
+  ovr_plot
+}
+
 pdf(here("plot_results", "bestperform_tileplot.pdf"), width = 13, height = 4.25)
-plot1 + plot2 +
-  plot_layout(guides = "collect")  &
-  theme(legend.position = "bottom")
-
+print(tileplot())
 dev.off()
 
+pdf(here("plot_results", "bestperform_tileplot_allhor.pdf"), width = 13, height = 4.25)
+print(tileplot(horizons = 1:4))
+dev.off()
 #############################ensemble size#####################################
 
 loctargets <- enscomb_specs$loctargets #exclude PL and DE for now, since not done yet
@@ -408,16 +431,16 @@ make_recomb_plot <- function(enstype, annotation = FALSE){
 }
 pdf(here("plot_results", paste0("ens_comb_", "median_ensemble",".pdf")), width = 12, height = 8)
 median_plot <- make_recomb_plot("median_ensemble")
-median_plot
+print(median_plot)
 dev.off()
 
 pdf(here("plot_results", paste0("ens_comb_", "mean_ensemble",".pdf")), width = 12, height = 8)
 mean_plot <- make_recomb_plot("mean_ensemble")
-mean_plot
+print(mean_plot)
 dev.off()
 
 
 pdf(here("plot_results", paste0("ens_comb_", "median_ensemble_annotated",".pdf")), width = 12, height = 8)
-mean_plot <- make_recomb_plot("median_ensemble", annotation = TRUE)
-mean_plot
+median_plot_ann <- make_recomb_plot("median_ensemble", annotation = TRUE)
+print(median_plot_ann)
 dev.off()
