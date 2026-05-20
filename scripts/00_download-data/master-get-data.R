3624116fdc8853d218b4dfd8ea61a44a4bc697af
fredbec <f-becker@posteo.de>
2026-05-20 11:56:25 +0200

    fix pipeline, zero handling

--- scripts/00_download-data/master-get-data.R
+++ scripts/00_download-data/master-get-data.R
@@ -1,5 +1,7 @@
 library(here)
 library(data.table)
+
+source(here("scripts", "00_download-data", "get-population.R"))
 source(here("scripts", "00_download-data", "get-truth.R"))
 source(here("scripts", "00_download-data", "get-data.R"))
 source(here("scripts", "00_download-data", "get-anomalies.R"))
--- scripts/01_load-data.R
+++ scripts/01_load-data.R
@@ -36,6 +36,7 @@ combdat <- fcdat |>
   filter(!model == "EuroCOVIDhub-ensemble") |>
   filter(forecast_date >= as.Date("2021-03-11")) |> #before: 2021-03-20
   filter(forecast_date <= as.Date("2023-03-11")) |>
+  DT(prediction<0, prediction := 0) |>
   mutate(forecast_date = as.IDate(forecast_date)) |>
   merge_forecasts_with_truth(truth) |>
   DT(, location_name := NULL) |>
